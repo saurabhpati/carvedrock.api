199bbdbe9b72eb5e57ec3518a384c4d52cdf8802
Saurabh Pati <saurabhpati.pati@gmail.com>
2019-05-20 21:11:37 +0530

    single product query.

--- CarvedRockApi/GraphQL/CarvedRockQuery.cs
+++ CarvedRockApi/GraphQL/CarvedRockQuery.cs
@@ -4,11 +4,18 @@ using GraphQL.Types;
 
 namespace CarvedRockApi.GraphQL
 {
-    public class CarvedRockQuery : ObjectGraphType  
+    public class CarvedRockQuery : ObjectGraphType
     {
         public CarvedRockQuery(IRepository<Product, int> productRepository)
         {
             Field<ListGraphType<GraphQL.Types.ProductType>>("products", resolve: context => productRepository.GetAllAsync());
+
+            Field<GraphQL.Types.ProductType>(
+                "product",
+                null,
+                new QueryArguments(
+                    new QueryArgument<NonNullGraphType<IdGraphType>>() { Name = "id" }), 
+                    resolve: context => productRepository.GetAsync(context.GetArgument<int>("id")));
         }
     }
 }
